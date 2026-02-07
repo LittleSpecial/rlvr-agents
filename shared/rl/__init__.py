70b8e3823ccb2329ee8ecdfd94c4850886c90207
Xu <1436324684@qq.com>
2026-02-07 22:16:30 +0800

    Investigate duplicate roll out Prom

--- shared/rl/__init__.py
+++
@@ -1,7 +0,0 @@
-# RL Algorithms for RLVR
-
-from .base_trainer import BaseTrainer, TrainerConfig
-from .grpo_trainer import GRPOTrainer
-from .advantage import AdvantageComputer
-
-__all__ = ['BaseTrainer', 'TrainerConfig', 'GRPOTrainer', 'AdvantageComputer']
